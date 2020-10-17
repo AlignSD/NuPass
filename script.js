87f9cf38954a5d8fb185fba6b3d3f5a5984d1f76
Evan Alioto <EvanJAlioto@gmail.com>
2020-10-17 01:51:00 -0700

    added if else statements for when user chooses no options or all options

--- script.js
+++ script.js
@@ -40,6 +40,8 @@ var confirmLower;
 // Password variables
 specialCharacters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
 alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+// Variables to hold arrays
 shift = [];
 password = [];
 
@@ -75,6 +77,18 @@ function generatePassword() {
       confirmUpper = confirm("Would you like Uppercase characters?");
       confirmLower = confirm("Would you like Lowercase characters?");
     };
+
+
+    // If user chooses no for all options
+    if (!confirmLower && !confirmSpecialCharacters &&       !confirmUpper) {
+      // Alerts user they choose no options //
+      choices = alert ("Please try selecting at least one option!");
+
+    }
+    // If user chooses all options
+    else if (confirmLower && confirmSpecialCharacters && confirmUpper) {
+      choices = specialCharacters.concat(alphabet, alphabet2);
+    }
 
 
   }
